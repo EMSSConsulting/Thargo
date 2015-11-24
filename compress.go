bee36badaba2f45ced008b75164fdbc4f241a1b5
Benjamin Pannell <admin@sierrasoftworks.com>
2015-11-24 15:21:25 +0200

    feat: Added AddIf method to allow granular exclusion of entries

--- compress.go
+++ compress.go
@@ -2,7 +2,22 @@ package thargo
 
 // Add will add a compression target to this archive.
 func (a *Archive) Add(target Target) error {
-	writer, err := a.writer()
+	return a.AddIf(target, func(entry Entry) bool {
+    return true
+  })
+}
+
+// EntryFilterFunc is used in conjunction with AddIf to
+// add entries to an archive based on a pre-condition.
+type EntryFilterFunc func(entry Entry) bool
+
+// AddIf allows you to apply a predicate filter to each
+// of the entries which would be included in your archive.
+// This can be used to ensure that duplicates are not
+// added, if that is a concern, or simply to be notified
+// of each file which was added to the archive.
+func (a *Archive) AddIf(target Target, predicate EntryFilterFunc) error {
+  writer, err := a.writer()
 	if err != nil {
 		return err
 	}
@@ -16,10 +31,12 @@ func (a *Archive) Add(target Target) error {
 	}
 
 	for _, entry := range entries {
-		err = writer.Write(entry)
-		if err != nil {
-			return err
-		}
+    if predicate(entry) {
+      err = writer.Write(entry)
+      if err != nil {
+        return err
+      }
+    }
 	}
 
 	return nil
