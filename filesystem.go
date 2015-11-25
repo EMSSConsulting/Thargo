23129c06c41b15f262b4fb9fc0805e1b67af90b6
Benjamin Pannell <admin@sierrasoftworks.com>
2015-11-25 14:10:46 +0200

    feat: Implicitly include all directory contents if only a directory name is specified

--- filesystem.go
+++ filesystem.go
@@ -2,9 +2,10 @@ package thargo
 
 import (
 	"os"
+	"path"
 	"path/filepath"
 
-	"github.com/bmatcuk/doublestar"
+	"github.com/EMSSConsulting/doublestar"
 )
 
 // FileSystemTarget provides a compression target for file system entries, which are searched
@@ -17,6 +18,28 @@ type FileSystemTarget struct {
 // Entries retrieves the list of compression entries which should be
 // included for this target.
 func (t *FileSystemTarget) Entries() ([]Entry, error) {
+	if filepath.IsAbs(t.Pattern) {
+		return t.forAbsolutePath(t.Pattern)
+	}
+  
+	basePath, err := filepath.Abs(t.Path)
+	if err != nil {
+		return nil, err
+	}
+
+	fullPath := path.Join(basePath, t.Pattern)
+
+	// Check if the file/folder exists, if it does then treat it as
+	// an absolute path (possibly enumerating a directory's contents)
+	_, err = os.Stat(fullPath)
+	if err != nil {
+		return t.forGlobPath(basePath, t.Pattern)
+	}
+
+	return t.forAbsolutePath(fullPath)
+}
+
+func (t *FileSystemTarget) forAbsolutePath(path string) ([]Entry, error) {
 	entries := []Entry{}
 
 	basePath, err := filepath.Abs(t.Path)
@@ -24,54 +47,49 @@ func (t *FileSystemTarget) Entries() ([]Entry, error) {
 		return nil, err
 	}
 
-	if filepath.IsAbs(t.Pattern) {
-		relativePath, err := filepath.Rel(basePath, t.Pattern)
-		if err != nil {
-			relativePath = t.Pattern
-		}
+	relativePath, err := filepath.Rel(basePath, path)
+	if err != nil {
+		relativePath = path
+	}
 
-		f, err := os.Stat(t.Pattern)
-		if err != nil && !os.IsNotExist(err) {
-			return nil, err
-		}
+	f, err := os.Stat(path)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
 
-		if err == nil {
-			entries = append(entries, &FileEntry{Name: relativePath, Path: t.Pattern, Info: f})
-		}
-
+	if err != nil {
 		return entries, nil
 	}
 
-	visit := func(path string, f os.FileInfo, err error) error {
-		// We don't care about directories, only files
-		if f.IsDir() {
-			return nil
-		}
-
-		relativePath, err := filepath.Rel(basePath, path)
-		if err != nil {
-			return err
-		}
-
-		if t.Pattern != "" {
-			if t.Pattern != relativePath {
-				matched, err := doublestar.Match(t.Pattern, filepath.ToSlash(relativePath))
-				if err != nil {
-					return err
-				}
-
-				if !matched {
-					return nil
-				}
-			}
-		}
-
-		entries = append(entries, &FileEntry{Name: relativePath, Path: path, Info: f})
-		return nil
+	// If we are given a directory, enumerate its contents recursively
+	if f.IsDir() {
+		return t.forGlobPath(path, "**")
 	}
 
-	if err := filepath.Walk(basePath, visit); err != nil {
+	entries = append(entries, &FileEntry{Name: relativePath, Path: path, Info: f})
+	return entries, nil
+}
+
+func (t *FileSystemTarget) forGlobPath(basePath, glob string) ([]Entry, error) {
+	entries := []Entry{}
+
+	matches, err := doublestar.Glob(basePath, glob)
+	if err != nil {
 		return nil, err
+	}
+
+	for _, match := range matches {
+		relativePath, err := filepath.Rel(basePath, match)
+		if err != nil {
+			return nil, err
+		}
+
+		f, err := os.Stat(match)
+		if err != nil {
+			return nil, err
+		}
+
+		entries = append(entries, &FileEntry{Name: relativePath, Path: match, Info: f})
 	}
 
 	return entries, nil
