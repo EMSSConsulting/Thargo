f773a17b4e128fd44544bd2f8c545fbe8d15d897
Benjamin Pannell <admin@sierrasoftworks.com>
2015-11-25 07:36:28 +0200

    feat: Support specifying absolute paths to files outside of cwd
    Files will be included using their full path as the name, which may not
    be particularly user friendly - so make sure this is what you want
    before you make use of this feature.

--- filesystem.go
+++ filesystem.go
@@ -3,7 +3,8 @@ package thargo
 import (
 	"os"
 	"path/filepath"
-  "github.com/bmatcuk/doublestar"
+
+	"github.com/bmatcuk/doublestar"
 )
 
 // FileSystemTarget provides a compression target for file system entries, which are searched
@@ -17,25 +18,51 @@ type FileSystemTarget struct {
 // included for this target.
 func (t *FileSystemTarget) Entries() ([]Entry, error) {
 	entries := []Entry{}
+
+	basePath, err := filepath.Abs(t.Path)
+	if err != nil {
+		return nil, err
+	}
+
+	if filepath.IsAbs(t.Pattern) {
+		relativePath, err := filepath.Rel(basePath, t.Pattern)
+		if err != nil {
+			relativePath = t.Pattern
+		}
+
+		f, err := os.Stat(t.Pattern)
+		if err != nil && !os.IsNotExist(err) {
+			return nil, err
+		}
+
+		if err == nil {
+			entries = append(entries, &FileEntry{Name: relativePath, Path: t.Pattern, Info: f})
+		}
+
+		return entries, nil
+	}
+
 	visit := func(path string, f os.FileInfo, err error) error {
 		// We don't care about directories, only files
 		if f.IsDir() {
 			return nil
 		}
 
-		relativePath, err := filepath.Rel(t.Path, path)
+		relativePath, err := filepath.Rel(basePath, path)
 		if err != nil {
 			return err
 		}
 
 		if t.Pattern != "" {
-			matched, err := doublestar.Match(t.Pattern, filepath.ToSlash(relativePath))
-			if err != nil {
-				return err
-			}
+			if t.Pattern != relativePath {
+				matched, err := doublestar.Match(t.Pattern, filepath.ToSlash(relativePath))
+				if err != nil {
+					return err
+				}
 
-			if !matched {
-				return nil
+				if !matched {
+					return nil
+				}
 			}
 		}
 
@@ -43,9 +70,7 @@ func (t *FileSystemTarget) Entries() ([]Entry, error) {
 		return nil
 	}
 
-	err := filepath.Walk(t.Path, visit)
-
-	if err != nil {
+	if err := filepath.Walk(basePath, visit); err != nil {
 		return nil, err
 	}
 
--- filesystem_test.go
+++ filesystem_test.go
@@ -1,6 +1,7 @@
 package thargo
 
 import (
+	"path/filepath"
 	"testing"
 )
 
@@ -34,5 +35,35 @@ func TestFileSystemTarget(t *testing.T) {
 
 	if header.AccessTime.IsZero() {
 		t.Errorf("Expected entry access time to be non-zero")
+	}
+}
+
+func TestFileSystemTargetAbsolutePattern(t *testing.T) {
+	absPath, err := filepath.Abs("README.md")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	target := FileSystemTarget{
+		Path:    "./",
+		Pattern: absPath,
+	}
+
+	entries, err := target.Entries()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(entries) != 1 {
+		t.Error("Expected one entry to be found for README's absolute path")
+	}
+
+	header, err := entries[0].Header()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if header.Name != "README.md" {
+		t.Errorf("Expected entry name to be README.md, got %s instead", header.Name)
 	}
 }
