cb33f761b1d6095e0db9419fc4e8ef40375c0eac
Benjamin Pannell <admin@sierrasoftworks.com>
2015-12-04 07:23:00 +0200

    feat: Added an AddFiltered function for more flexibility in archive creation

--- compress_test.go
+++ compress_test.go
@@ -22,4 +22,59 @@ func TestAddToArchive(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	reader, err := core.reader()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	header, _ := reader.Next()
+	if header == nil {
+		t.Errorf("Expected reader to read a header from the archive")
+	}
+}
+
+func TestAddIf(t *testing.T) {
+	options := *DefaultOptions
+	options.CreateIfMissing = true
+
+	buf := new(bytes.Buffer)
+
+	core := NewArchive(buf, &options)
+
+	target := &StringTarget{
+		Name:    "test.txt",
+		Content: "test",
+	}
+
+	err := core.AddIf(target, func(entry Entry) bool {
+		return true
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reader, err := core.reader()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	header, _ := reader.Next()
+	if header == nil {
+		t.Errorf("Expected reader to read a header from the archive")
+	}
+
+	err = core.AddIf(target, func(entry Entry) bool {
+		return false
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	header, _ = reader.Next()
+	if header != nil {
+		t.Errorf("Expected no additional headers to be available in the archive")
+	}
 }
